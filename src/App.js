43dbdff64ab5db4d445c9871d214a42e461c0990
Katarzyna Mazur <mazur66@hotmail.com>
2018-09-25 00:16:32 -0400

    Created functions renderMap, initMap, loadScript

--- src/App.js
+++ src/App.js
@@ -3,19 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.renderMap()
+  }
+
+  renderMap = () => {
+      loadScript('https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap')
+      window.initMap = this.initMap
+
+  }
+
+  initMap = () => {
+   let map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 42.085, lng: -76.053},
+      zoom: 8    });
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main> 
+       <div id='map'></div>
+      </main>
     );
   }
 }
+
+/*  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+    async defer></script> */
+function loadScript(url){
+  let index =
+  window.document.getElementByTagName('script')[0]
+  let script = window.document.createElement('script')
+  script.src = url
+  script.async = true
+  script.differ = true
+  index.parentNode.index.insertBefore(script,index)
+}
+
+/*<script src = "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+async differ></script>*/
 
 export default App;
