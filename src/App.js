6829ce18b21ae2399a317556e4b56dda5d8e5489
Katarzyna Mazur <mazur66@hotmail.com>
2018-09-26 00:23:29 -0400

    Defined getVenues function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
 
   componentDidMount() {
+    this.getVenues()
     this.renderMap()
   }
 
   renderMap = () => {
-      loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAElGHukbd2Mb_rerPu-t6g2lvbkn77HYs&callback=initMap')
-      window.initMap = this.initMap
+    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAElGHukbd2Mb_rerPu-t6g2lvbkn77HYs&callback=initMap')
+    window.initMap = this.initMap
 
+  }
+
+  getVenues =() => {
+    let endPoint = 'https://api.foursquare.com/v2/venues/explore?'
+    let parameters = { 
+      client_id: 'II5QZS2CAQN5TUQXLAHUSHAIYNBWXHRZWIALEU0U15GISYB1',
+      client_secret: 'QXMSTGVV25LKDD4KFJ3JCX51HHB3Y5CN3CBKA2O5V5WKAZBE',
+      query: 'food',
+      near: 'Ithaca',
+      v: '20182507'
+    }
+    axios.get(endPoint + new URLSearchParams(parameters))
+          .then(response => {
+            console.log(response.data.response.groups[0].items)
+          })
+          .catch(error => {
+            console.log('ERROR' + error)
+          })
   }
 
   initMap = () => {
    let map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 42.085, lng: -76.053},
+      center: {lat: 42.443, lng: -76.501},
       zoom: 14 });
   }
 
