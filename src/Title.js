0ba1b73c78f0433460fe4d8ae7472d39786f069d
Katarzyna Mazur <mazur66@hotmail.com>
2018-09-30 10:25:54 -0400

    Added Title component

---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+    return(
+        <header id = 'header'>
+            <h1 className ='map-title'>Restaurants in Ithaca, NY</h1>
+        </header>
+    )
+}
+
+export default Title;
