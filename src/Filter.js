8c374135bfb6a47fa339b7a3599afb589289d4ad
Katarzyna Mazur <mazur66@hotmail.com>
2018-10-03 00:21:04 -0400

    Added code to Filter

--- src/Filter.js
+++ src/Filter.js
@@ -3,47 +3,22 @@ import axios from 'axios'
 
 
 class Filter extends Component {
-    constructor () {
-        super();
-        this.state = {venues:[]}
+    constructor (props) {
+        super(props)
+        this.state ={
+            filter:''
+        }
     }
 
-    componentDidMount(){
-        this.getVenues();
-    }
-
-    getVenues =() => {
-        let endPoint = 'https://api.foursquare.com/v2/venues/explore?'
-        let parameters = { 
-          client_id: 'II5QZS2CAQN5TUQXLAHUSHAIYNBWXHRZWIALEU0U15GISYB1',
-          client_secret: 'QXMSTGVV25LKDD4KFJ3JCX51HHB3Y5CN3CBKA2O5V5WKAZBE',
-          query: 'food',
-          near: 'Ithaca',
-          v: '20182507'
-        } 
-
-    axios.get(endPoint + new URLSearchParams(parameters))
-    .then(response => {
-    this.setState({
-        venues: response.data.response.groups[0].items})
-    })
-    .catch(error => {
-        console.log('ERROR' + error)
-    })
-}    
-
+   
     filter(e){
         this.setState({filter:e.target.value})
+        this.props.filter(e.target.value)
     }
     render (){
-        let venues = this.state.venues //.slice(0,10);
+        let venues = this.state.venues.slice(0,10);
    
-        if(this.state.filter){
-            venues=venues.filter(myVenue=>
-                myVenue.venue.name.toLowerCase()
-                .includes(this.state.filter.toLowerCase()))
-        }
-        return(
+            return(
             <div className='side-container'>
                 <input className='input-container' type='text'
                      onChange={this.filter.bind(this)}/>
@@ -56,9 +31,9 @@ class Filter extends Component {
                 </div>    
         
             </div>
-            )
-            }
-        }
+        )
+    }
+}
 
 
 export default Filter;
