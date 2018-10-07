c6d961d7c50904e4f9d37b89d2650652b827157b
Katarzyna Mazur <mazur66@hotmail.com>
2018-10-06 23:50:00 -0400

    Cleaning the code

--- src/Filter.js
+++ src/Filter.js
@@ -2,14 +2,13 @@ import React, {Component} from 'react';
 
 class Filter extends Component {
 
-
     handleClicks(venue) {
         const {infowindow, map, contents, markers} = this.props;
         // filter the markers that match the specific location name then onClick show info window
         markers.filter(marker => marker.title === venue.name).forEach(marker => {
             infowindow.setContent(String(contents.filter(content => String(content).slice(10).includes(venue.name))));
             infowindow.open(map, marker);
-            // set bounce animation for the marker that is clicked
+            // set  animation for the marker that is clicked
             marker.setAnimation(window.google.maps.Animation.BOUNCE)
                 setTimeout(function() {
                 marker.setAnimation(null)
@@ -26,12 +25,10 @@ class Filter extends Component {
     let query = this.props.query;
     return (
             <div  className = {`restaurant-list ${this.props.listClass}`}  aria-label='Restaurants in Ithaca, NY'  >
-                
                 <ul>           
                     {venues.filter(venue=>
-                    
-                    venue.name.toLowerCase().includes(query.toLowerCase())).map((venue, index) => {
-                            return <li className="list-item" aria-labelledby = 'restaruant name'key={index} onClick={this.handleClicks.bind(this, venue)} tabIndex={0}>{venue.name}</li>})
+                        venue.name.toLowerCase().includes(query.toLowerCase())).map((venue, index) => {
+                        return <li className="list-item" aria-labelledby = 'restaruant name'key={index} onClick={this.handleClicks.bind(this, venue)} tabIndex={0}>{venue.name}</li>})
                     }                   
                 </ul>
             </div>
